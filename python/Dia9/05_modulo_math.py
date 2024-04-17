87ad892ea08bde22e9e73196e0ce27ae3d63da15
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-04-17 01:58:52 -0600

    Se agrega ejercicio 5

---
+++ python/Dia9/05_modulo_math.py
@@ -0,0 +1,17 @@
+'''
+El módulo math contiene un conjunto de métodos y
+constantes que se pueden utilizar para resolver tareas
+matemáticas de mayor complejidad. Es el equivalente a la
+calculadora científica dentro de Python.
+'''
+
+import math
+
+#Este redondea hacia abajo
+resultado = math.floor(89.455)
+print(resultado)
+
+#Este redondea hacia arriba
+resultado2 = math.ceil(89.455)
+print(resultado2)
+
