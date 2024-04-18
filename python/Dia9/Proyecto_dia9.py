668c816f6c12217f0924e55cd9a6afa06f9acbfe
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-04-18 06:43:39 -0600

    Se agrega proyecto9 mejorado

--- python/Dia9/Proyecto_dia9.py
+++ python/Dia9/Proyecto_dia9.py
@@ -5,14 +5,10 @@ import datetime
 import math
 
 ruta = 'C:\\Users\\yo_al\\Documentos\\PythonTotal\\python\\Dia9\\Proyecto+Dia+9'
-
 patron = r'N\D{3}-\d{5}'
-
 dia_actual = datetime.date.today()
-
 series = []
 
-inicio = time.time()
 
 #Walk recorre todos los directorios, carpetas, subcarpetas y archivos que encuentre en su camino dada una ruta
 print('-' * 80)
@@ -20,20 +16,23 @@ print(f'Fecha de busqueda: [{dia_actual}]\n')
 print('ARCHIVO\t\tNRO. SERIE')
 print('-------\t\t----------')
 
+inicio = time.time()
 for carpetas, subcarpetas, archivos in os.walk(ruta):
+    
     for archivo in archivos:
         ruta_archivo = os.path.join(carpetas, archivo)
+        
         with open(ruta_archivo, 'r') as archivo_abierto:
-
             contenido = archivo_abierto.readlines()
+            
             for linea in contenido:
                 coincidencia = re.search(patron, linea)
                 if coincidencia:
                     print(f'{archivo}\t{coincidencia.group()}')
                     if coincidencia:
                         series.append(coincidencia.group())
-
 final = time.time()
+
     
 tiempo_ejecucion = (final - inicio)   
                  
