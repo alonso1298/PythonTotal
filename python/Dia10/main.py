111a755a41e6856d0541a4ea7738cfb5da6841dc
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-04-19 03:32:00 -0600

    Limitar bordes

--- python/Dia10/main.py
+++ python/Dia10/main.py
@@ -14,13 +14,15 @@ pygame.display.set_caption('Invasion Espacial')
 icono = pygame.image.load('C:\\Users\\yo_al\\Documentos\\PythonTotal\\python\\Dia10\\nasa.png')
 pygame.display.set_icon(icono)
 
-#Jugador
+# Varibles del Jugador
 img_jugador = pygame.image.load('C:\\Users\\yo_al\\Documentos\\PythonTotal\\python\\Dia10\\nave-espacial.png')
-img_jugador_x = 368
-img_jugador_y = 536
+jugador_x = 368
+jugador_y = 536
 jugadorx_cambio = 0
 jugadory_cambio = 0
 
+
+#Funcion Jugador 
 def jugador(x, y):
     pantalla.blit(img_jugador, (x, y))
 
@@ -34,23 +36,39 @@ while se_ejecuta:
     #RGB pantalla
     pantalla.fill((205, 144, 228))
     
+    #Iterar eventos
     for evento in pygame.event.get():
+        
+        # Evento Cerrar programa
         if evento.type == pygame.QUIT:
             se_ejecuta = False
-            
+        
+        #Evento presionar flechas    
         if evento.type == pygame.KEYDOWN:
             if evento.key == pygame.K_LEFT:
                 jugadorx_cambio = -0.3
             if evento.key == pygame.K_RIGHT:
                 jugadorx_cambio = 0.3
-                
+        
+        #Evento Soltar flechas        
         if evento.type == pygame.KEYUP:
             if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
                 jugadorx_cambio = 0
     
-    img_jugador_x += jugadorx_cambio
-    jugador(img_jugador_x, img_jugador_y)
+    #Modificar ubicacion
+    jugador_x += jugadorx_cambio
     
+    #Mantener dentro de bordes 
+    if jugador_x <= 0:
+        jugador_x = 0
+        
+    elif jugador_x >= 736:
+        jugador_x = 736
+        
+        
+    jugador(jugador_x, jugador_y)
+    
+    #Actualizar
     pygame.display.update()
 
 
